f593dc74a10d57aa3eef7246484b6d24ca2ae176
tutkumelek <karan.tutkumelek@ogr.ahievran.edu.tr>
2023-09-17 16:55:46 +0300

    first commit

---
+++ questions.js
@@ -0,0 +1,49 @@
+let questions = [
+  {
+    numb: 1,
+    question: " HTML etiketleri hangi karakterlerle belirtilir?",
+    answer: "d) <> ",
+    options: ["a) !", "b) ( )", "c) #", "d) <> "],
+  },
+  {
+    numb: 2,
+    question: "Javascript ile konsola nasıl bilgi yazdırabilirsiniz?",
+    answer: "b)console.log(info) ",
+    options: [
+      "a)console(info)",
+      "b)console.log(info) ",
+      "c(print(info) ",
+      "d)printf(info)",
+    ],
+  },
+
+  {
+    numb: 3,
+    question: "JavaScript'te bir 'if' ifadesi nasıl yazılır?",
+    answer: "c)if (i == 5)",
+    options: [
+      "a)if i = 5 then",
+      "b)if i == 5 then",
+      "c)if (i == 5)",
+      "d)if i === 5 then",
+    ],
+  },
+
+  {
+    numb: 4,
+    question: "JavaScript'i kodlarını hangi iki etiket arasına yazarız ?",
+    answer: "d)script",
+    options: ["a)js", "b)jsx", "c)javascript", "d)script"],
+  },
+  {
+    numb: 5,
+    question: "12.35 sayısını en yakın tam sayıya nasıl yuvarlarsınız?",
+    answer: "b)Math.round(12.35)",
+    options: [
+      "a)Math.rnd(12.35)",
+      "b)Math.round(12.35)",
+      "c)round(12.35)",
+      "d)rounded(12.35)",
+    ],
+  },
+];
